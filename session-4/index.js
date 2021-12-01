e1a76e362d880d54e9c2bdf08bc35d8a1f6e3cbf
karacodes88 <karawhite59@gmail.com>
2021-12-01 20:57:25 +0000

    task-1-3

--- session-4/index.js
+++ session-4/index.js
@@ -1,9 +1,68 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
 
+let personA = {
+    name: 'kara', 
+    likes: 'apples', 
+    age: '20',
+    location: 'birmingham'
+};
 
+console.log(personA);
 
+console.log(personA.name)
 
+const personB = {
+    name: 'kawther',
+    likes: 'mangoes',
+    age: '25',
+    location: 'tyseley'
+};
 
+console.log(personB);
+
+console.log(personB.location)
+
+let personC = {
+    name: 'ayo',
+    likes: 'berries',
+    age: '19',
+    location: 'harbone'
+};
+
+console.log(personC);
+
+console.log(personC.age)
+
+let personD = {
+    name: 'lucy',
+    likes: 'football',
+    age: '23',
+    location: 'newtown'
+};
+
+console.log(personD);
+
+console.log(personD.likes)
+
+function biography(person){
+    return(
+        " hello my name is " +
+        person.name +
+        ". I am " +
+        person.age +
+        " years old, I live in " +
+        person.location +
+        " and I like " +
+        person.likes
+    );
+}
+
+console.log(biography(personA));
+console.log(biography(personB));
+console.log(biography(personC));
+console.log(biography(personD));
+
+// 
 
 
 
@@ -19,24 +78,35 @@
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 8;
+const y = 12;
 
-// const addition = x + y;
+const addition = x * y;
 
-// console.log("Addition: x + y " + addition);
+const multiply = x * y
 
-// const subtraction = x - y;
+console.log("multiply: x * y " + multiply);
 
-// console.log("Subtraction: x - y " + subtraction);
+const subtraction = x - y;
 
-// const multiplication = x * y;
+console.log("Subtraction: x - y " + subtraction);
 
-// console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
 
-// console.log("Division: x + y " + division);
+console.log("multiplication: x * y " + multiply);
+
+const division = x * y;
+
+console.log("Division: x + y " + division);
+
+
+const mentors = 35;
+const age = 17;
+const students = 16;
+
+const isAgeLimit =( mentors + students) < age;
+
+console.log('do students reach the age? ' + isAgeLimit);
 
 
 
