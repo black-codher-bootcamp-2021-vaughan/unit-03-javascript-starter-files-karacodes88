f6c3e92ca74021a5d7b799f4a34d514531d124a9
karacodes88 <karawhite59@gmail.com>
2021-12-08 22:43:02 +0000

    task-1-2-loop

--- session-6/index.js
+++ session-6/index.js
@@ -1,13 +1,16 @@
-// Use this randomNumber inside of the while/for loops to generate a random number
-const randomNumber = Math.round(Math.random() * 10);
+// // Use this randomNumber inside of the while/for loops to generate a random number
+// const randomNumber = Math.round(Math.random() * 10);
 
 // ****************************************************TASK 1****************************************************
 
+// let counter = 1;
+// // let randomNumber = 0;
 
-
-
-
-
+// while ( counter <=10){
+//     counter = counter +1;
+//     const randomNumber = Math.round(Math.random() * 10);
+//     console.log (randomNumber)
+// }
 
 
 
@@ -20,7 +23,13 @@ const randomNumber = Math.round(Math.random() * 10);
 // ****************************************************TASK 2****************************************************
 
 
+// let randomNumber = 0;
 
+for  ( let counter = 0; counter <=10; counter = counter + 1){
+   
+    const randomNumber = Math.round(Math.random() * 10);
+    console.log (randomNumber);
+}
 
 
 
