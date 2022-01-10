54053e06b68c7ecef94fafbededb5cc9c524f727
karacodes88 <karawhite59@gmail.com>
2022-01-10 12:52:04 +0000

    task1-2

--- session-5/index.js
+++ session-5/index.js
@@ -1,22 +1,22 @@
-   //  function getGrade (total){
-   //    if (total >=90) {
-   // console.log('Well done, your grade is A+')
+    function getGrade (total){
+      if (total >=90) {
+   console.log('Well done, your grade is A+')
     
-   //    } else if (total >=80 && total < 90){
-   // console.log ('')
-   //    } else if (total >= 70 && total <80){
-   // console.log (' grade B ')
-   //    }else if (total >= 60 && total <70){
-   // console.log (' grade C ') 
-   //    }else if (total >=50 && total <60){
-   // console.log ('grade D')      
-   //    }
-   // }
+      } else if (total >=80 && total < 90){
+   console.log ('')
+      } else if (total >= 70 && total <80){
+   console.log (' grade B ')
+      }else if (total >= 60 && total <70){
+   console.log (' grade C ') 
+      }else if (total >=50 && total <60){
+   console.log ('grade D')      
+      }
+   }
       
-   //getGrade(95)
-   //getGrade(75)
-   //getGrade(50)
-   //getGrade(65)
+   getGrade(95)
+   getGrade(75)
+   getGrade(50)
+   getGrade(65)
 
 // task 2
 
